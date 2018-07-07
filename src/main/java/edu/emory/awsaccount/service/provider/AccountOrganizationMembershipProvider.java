f6d48fe525046b4099ae35afe64e5f198c4bb1cc
george wang <george.wang@emory.edu>
2018-07-07 12:01:52 -0400

    new aws-moas

--- src/main/java/edu/emory/awsaccount/service/provider/AccountOrganizationMembershipProvider.java
+++ src/main/java/edu/emory/awsaccount/service/provider/AccountOrganizationMembershipProvider.java
@@ -11,74 +11,11 @@
 
 package edu.emory.awsaccount.service.provider;
 
-// Core Java
-import java.util.List;
-
-// OpenEAI config foundation
-import org.openeai.config.AppConfig;
+import org.openeai.jms.consumer.commands.provider.CrudProvider;
 
 import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountOrganizationMembership;
 import com.amazon.aws.moa.objects.resources.v1_0.AccountOrganizationMembershipQuerySpecification;
 
-// AWS Message Object API (MOA)
-
-//import com.amazon.aws.moa.objects.resources.v1_0.StackRequisition;
-
-
-/**
- * Interface for all CloudFormation Stack object providers.
- * <P>
- * 
- * @author Steve Wheat (swheat@emory.edu)
- * @version 1.0 - 25 December 2016
- */
-public interface AccountOrganizationMembershipProvider {
-    /**
-     * 
-     * <P>
-     * 
-     * @param AppConfig
-     *            , an AppConfig object with all this provider needs.
-     *            <P>
-     * @throws ProviderException
-     *             with details of the initialization error.
-     */
-    public void init(AppConfig aConfig) throws ProviderException;
-
-    /**
-     * 
-     * <P>
-     * 
-     * @param StackQuerySpecficiation, the query parameter.
-     * @return List, a list of matching Stack objects.
-     *         <P>
-     * @throws ProviderException
-     *             with details of the providing the list.
-     */
-    public List<AccountOrganizationMembership> query(AccountOrganizationMembershipQuerySpecification querySpec) 
-    	throws ProviderException;  
-
-    /**
-     * 
-     * <P>
-     * 
-     * @param StackRequisition, the generate parameter.
-     * @return Stack, a generated Stack for the requisition.
-     *         <P>
-     * @throws ProviderException with details of the error generating the stack.
-     */
-    public void create(AccountOrganizationMembership requisition) 
-    	throws ProviderException;
-    
-    
-    /**
-     * 
-     * <P>
-     * 
-     * @param Stack, the object to delete.
-     *            <P>
-     * @throws ProviderException with details of the error deleting the stack.
-     */
-    public void delete(AccountOrganizationMembership stack) throws ProviderException;
-
+public interface AccountOrganizationMembershipProvider
+        extends CrudProvider<AccountOrganizationMembership, AccountOrganizationMembershipQuerySpecification> {
 }
--- src/main/java/edu/emory/awsaccount/service/provider/AwsAccountAliasProvider.java
+++ src/main/java/edu/emory/awsaccount/service/provider/AwsAccountAliasProvider.java
@@ -11,32 +11,18 @@
 
 package edu.emory.awsaccount.service.provider;
 
-// Java utilities
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
 
 // Log4j
 import org.apache.log4j.Category;
-
-// JDOM
-import org.jdom.Document;
-import org.jdom.Element;
-
 // OpenEAI foundation
 import org.openeai.OpenEaiObject;
 import org.openeai.config.AppConfig;
-import org.openeai.config.EnterpriseConfigurationObjectException;
-import org.openeai.config.EnterpriseFieldException;
-import org.openeai.config.PropertyConfig;
-import org.openeai.layouts.EnterpriseLayoutException;
-import org.openeai.xml.XmlDocumentReader;
-import org.openeai.xml.XmlDocumentReaderException;
+import org.openeai.jms.consumer.commands.provider.AbstractCrudProvider;
+import org.openeai.jms.consumer.commands.provider.ProviderException;
 
 import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountAlias;
 import com.amazon.aws.moa.objects.resources.v1_0.AccountAliasQuerySpecification;
-
 //AWS Message Object API (MOA)
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
@@ -45,44 +31,17 @@ import com.amazonaws.services.identitymanagement.model.CreateAccountAliasResult;
 import com.amazonaws.services.identitymanagement.model.DeleteAccountAliasRequest;
 import com.amazonaws.services.identitymanagement.model.DeleteAccountAliasResult;
 import com.amazonaws.services.identitymanagement.model.ListAccountAliasesRequest;
-import com.amazonaws.services.identitymanagement.model.ListAccountAliasesResult;
 
-/**
- *
- * @author Steve Wheat (swheat@emory.edu)
- *
- */
-public class AwsAccountAliasProvider extends OpenEaiObject implements AccountAliasProvider {
+public class AwsAccountAliasProvider extends AbstractCrudProvider<AccountAlias, AccountAliasQuerySpecification> {
 
     private Category logger = OpenEaiObject.logger;
     private AppConfig m_appConfig;
-    private String m_provideReplyUri = null;
-    private String m_responseReplyUri = null;
-    private long m_stackId = 2646351098L;
+
     private String LOGTAG = "[AwsAccountAliasProvider] ";
     private AmazonIdentityManagement amazonIdentityManagement = AmazonIdentityManagementClientBuilder.defaultClient();
 
-    @Override
-    public void init(AppConfig aConfig) throws ProviderException {
-        logger.info(LOGTAG + "Initializing...");
-        m_appConfig = aConfig;
-
-        // Get the provider properties
-        PropertyConfig pConfig = new PropertyConfig();
-        try {
-            pConfig = (PropertyConfig) aConfig.getObject("AwsAccountAliasProviderProperties");
-        } catch (EnterpriseConfigurationObjectException eoce) {
-            String errMsg = "Error retrieving a PropertyConfig object from " + "AppConfig: The exception is: " + eoce.getMessage();
-            logger.error(LOGTAG + errMsg);
-            throw new ProviderException(errMsg, eoce);
-        }
-
-        logger.info(LOGTAG + pConfig.getProperties().toString());
-
-        logger.info(LOGTAG + "Initialization complete.");
-    }
-
     /**
+     * @throws ProviderException
      * @see AccountAliasProvider.java
      * 
      *      Note: this implementation queries by StackId.
@@ -93,7 +52,7 @@ public class AwsAccountAliasProvider extends OpenEaiObject implements AccountAli
         // If the StackId is null, throw an exception.
         if (querySpec.getAccountId() == null || querySpec.getAccountId().equals("")) {
             String errMsg = "The StackId is null. The ExampleAccountAliasProvider" + "presently only implements query by StackId.";
-            throw new ProviderException(errMsg);
+            throw new org.openeai.jms.consumer.commands.provider.ProviderException(errMsg);
         }
 
         ListAccountAliasesRequest request = new ListAccountAliasesRequest();
@@ -112,7 +71,7 @@ public class AwsAccountAliasProvider extends OpenEaiObject implements AccountAli
      * @see AccountAliasProvider.java
      */
     @Override
-    public void create(AccountAlias req) throws ProviderException {
+    public void create(AccountAlias req) throws org.openeai.jms.consumer.commands.provider.ProviderException {
         try {
             // samlProvider = (AccountAlias)
             // m_appConfig.getObjectByType(samlProvider.getClass().getName());
@@ -123,33 +82,15 @@ public class AwsAccountAliasProvider extends OpenEaiObject implements AccountAli
         } catch (Exception ecoe) {
             String errMsg = "An error occurred retrieving an object from " + "AppConfig. The exception is: " + ecoe.getMessage();
             logger.error(LOGTAG + errMsg);
-            throw new ProviderException(errMsg, ecoe);
+            throw new org.openeai.jms.consumer.commands.provider.ProviderException(errMsg, ecoe);
         }
-
-        // Add the Stack to the map.
-        // m_stackMap.put(req.getAccountId(), req);
-    }
-
-    /**
-     * @see AccountAliasProvider.java
-     */
-    public void update(AccountAlias samlProvider) throws ProviderException {
-        // TODO
-        // use delette and create
-        // UpdateAccountAliasRequest request = new UpdateSAMLProviderRequest();
-        // // TODO: samlProvier to request
-        // UpdateSAMLProviderResult result =
-        // amazonIdentityManagement.updateSAMLProvider(request);
-        // Replace the object in the map with the same StackId.
-        // TODO: check result
-        return;
     }
 
     /**
      * @see AccountAliasProvider.java
      */
     @Override
-    public void delete(AccountAlias stack) throws ProviderException {
+    public void delete(AccountAlias stack) throws org.openeai.jms.consumer.commands.provider.ProviderException {
 
         DeleteAccountAliasRequest request = new DeleteAccountAliasRequest();
         // TODO: samlProvier to request
@@ -158,45 +99,5 @@ public class AwsAccountAliasProvider extends OpenEaiObject implements AccountAli
         // TODO: check result));
 
         return;
-    }
-
-    /**
-     * @param String,
-     *            the URI to a provide reply document containing a sample
-     *            object.
-     *            <P>
-     *            This method sets the provide reply URI property
-     */
-    private void setProvideReplyUri(String provideReplyUri) {
-        m_provideReplyUri = provideReplyUri;
-    }
-
-    /**
-     * @return String, the provide reply document containing a sample object
-     *         <P>
-     *         This method returns the value of the provide reply URI property
-     */
-    private String getProvideReplyUri() {
-        return m_provideReplyUri;
-    }
-
-    /**
-     * @param String,
-     *            the URI to a response reply document containing a sample
-     *            object.
-     *            <P>
-     *            This method sets the provide reply URI property
-     */
-    private void setResponseReplyUri(String responseReplyUri) {
-        m_responseReplyUri = responseReplyUri;
-    }
-
-    /**
-     * @return String, the response reply document containing a sample object
-     *         <P>
-     *         This method returns the value of the response reply URI property
-     */
-    private String getResponseReplyUri() {
-        return m_responseReplyUri;
     }
 }
