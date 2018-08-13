a147b5e8a4f1db59dfd02ed6361ccff1d0bd4426
Stephen Wheat <swheat@serviceforge.net>
2018-08-12 22:43:13 -0400

    added steps

---
+++ src/main/java/edu/emory/awsaccount/service/provider/step/CreateRsAccountCfnStack.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ $Source: $
+ $Revision: $
+ *******************************************************************************/
+
+/******************************************************************************
+ This file is part of the Emory AWS Account Service.
+
+ Copyright (C) 2017 Emory University. All rights reserved. 
+ ******************************************************************************/
+package edu.emory.awsaccount.service.provider.step;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.openeai.config.AppConfig;
+
+import com.amazon.aws.moa.objects.resources.v1_0.Property;
+import com.amazon.aws.moa.objects.resources.v1_0.VirtualPrivateCloudRequisition;
+
+import edu.emory.awsaccount.service.provider.VirtualPrivateCloudProvisioningProvider;
+
+/**
+ * Example step that can serve as a placholder.
+ * <P>
+ * 
+ * @author Steve Wheat (swheat@emory.edu)
+ * @version 1.0 - 21 May 2017
+ **/
+public class CreateRsAccountCfnStack extends AbstractStep implements Step {
+
+	public void init (String provisioningId, Properties props, 
+			AppConfig aConfig, VirtualPrivateCloudProvisioningProvider vpcpp) 
+			throws StepException {
+		
+		super.init(provisioningId, props, aConfig, vpcpp);
+	}
+	
+	protected List<Property> run() throws StepException {
+		long startTime = System.currentTimeMillis();
+		String LOGTAG = getStepTag() + "[ExampleStep.run] ";
+		logger.info(LOGTAG + "Begin running the step.");
+		
+		// Wait some time.
+		try {
+			Thread.sleep(5000);
+		}
+		catch (InterruptedException ie) {
+			String errMsg = "Error occurred sleeping.";
+			logger.error(LOGTAG + errMsg + ie.getMessage());
+			throw new StepException(errMsg, ie);
+		}
+		
+		// Set return properties.
+		ArrayList<Property> props = new ArrayList<Property>();
+		props.add(buildProperty("stepExecutionMethod", RUN_EXEC_TYPE));
+		
+		// Update the step.
+    	update(COMPLETED_STATUS, SUCCESS_RESULT, props);
+    	
+    	// Log completion time.
+    	long time = System.currentTimeMillis() - startTime;
+    	logger.info(LOGTAG + "Step run completed in " + time + "ms.");
+    	
+    	// Return the properties.
+    	return props;
+    	
+	}
+	
+	protected List<Property> simulate() throws StepException {
+		long startTime = System.currentTimeMillis();
+		String LOGTAG = getStepTag() + 
+			"[ExampleStep.simulate] ";
+		logger.info(LOGTAG + "Begin step simulation.");
+		
+		// Set return properties.
+		ArrayList<Property> props = new ArrayList<Property>();
+		Property prop = buildProperty("allocateNewAccount", "true");
+    	props.add(buildProperty("stepExecutionMethod", SIMULATED_EXEC_TYPE));
+		
+		// Update the step.
+    	update(COMPLETED_STATUS, SUCCESS_RESULT, props);
+    	
+    	// Log completion time.
+    	long time = System.currentTimeMillis() - startTime;
+    	logger.info(LOGTAG + "Step simulation completed in " + time + "ms.");
+    	
+    	// Return the properties.
+    	return props;
+	}
+	
+	protected List<Property> fail() throws StepException {
+		long startTime = System.currentTimeMillis();
+		String LOGTAG = getStepTag() + 
+			"[ExampleStep.fail] ";
+		logger.info(LOGTAG + "Begin step failure simulation.");
+		
+		// Set return properties.
+		ArrayList<Property> props = new ArrayList<Property>();
+    	props.add(buildProperty("stepExecutionMethod", FAILURE_EXEC_TYPE));
+		
+		// Update the step.
+    	update(COMPLETED_STATUS, FAILURE_RESULT, props);
+    	
+    	// Log completion time.
+    	long time = System.currentTimeMillis() - startTime;
+    	logger.info(LOGTAG + "Step failure simulation completed in " + time + "ms.");
+    	
+    	// Return the properties.
+    	return props;
+	}
+	
+	public void rollback() throws StepException {
+		long startTime = System.currentTimeMillis();
+		String LOGTAG = getStepTag() + 
+			"[ExampleStep.rollback] ";
+		logger.info(LOGTAG + "Rollback called, but this step has nothing to " + 
+			"roll back.");
+		update(ROLLBACK_STATUS, SUCCESS_RESULT, null);
+		
+		// Log completion time.
+    	long time = System.currentTimeMillis() - startTime;
+    	logger.info(LOGTAG + "Rollback completed in " + time + "ms.");
+	}
+	
+}
