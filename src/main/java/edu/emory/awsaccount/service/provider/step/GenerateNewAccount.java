f6c8eb8e8b2feebc79ff631074e452906050eb9b
Stephen Wheat <swheat@serviceforge.net>
2018-08-13 14:39:45 -0400

    renamed step implementation

--- src/main/java/edu/emory/awsaccount/service/provider/step/GenerateNewAccount.java
+++ src/main/java/edu/emory/awsaccount/service/provider/step/GenerateNewAccount.java
@@ -28,7 +28,7 @@ import edu.emory.awsaccount.service.provider.VirtualPrivateCloudProvisioningProv
  * @author Steve Wheat (swheat@emory.edu)
  * @version 1.0 - 21 May 2017
  **/
-public class GereateNewAccount extends AbstractStep implements Step {
+public class GenerateNewAccount extends AbstractStep implements Step {
 
 	public void init (String provisioningId, Properties props, 
 			AppConfig aConfig, VirtualPrivateCloudProvisioningProvider vpcpp) 
