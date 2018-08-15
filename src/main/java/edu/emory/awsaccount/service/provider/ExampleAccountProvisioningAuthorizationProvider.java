35c37873b8de04ab38a25885f75c2653413dc4cd
Stephen Wheat <swheat@serviceforge.net>
2018-08-15 14:24:28 -0400

    clean up and logging

--- src/main/java/edu/emory/awsaccount/service/provider/ExampleAccountProvisioningAuthorizationProvider.java
+++ src/main/java/edu/emory/awsaccount/service/provider/ExampleAccountProvisioningAuthorizationProvider.java
@@ -15,15 +15,8 @@ package edu.emory.awsaccount.service.provider;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Random;
-
 // Log4j
 import org.apache.log4j.Category;
-
-// JDOM
-import org.jdom.Document;
-import org.jdom.Element;
 
 // OpenEAI foundation
 import org.openeai.OpenEaiObject;
@@ -31,23 +24,10 @@ import org.openeai.config.AppConfig;
 import org.openeai.config.EnterpriseConfigurationObjectException;
 import org.openeai.config.EnterpriseFieldException;
 import org.openeai.config.PropertyConfig;
-import org.openeai.jms.consumer.commands.provider.ExampleCrudProvider;
-import org.openeai.layouts.EnterpriseLayoutException;
-import org.openeai.xml.XmlDocumentReader;
-import org.openeai.xml.XmlDocumentReaderException;
-
 //AWS Message Object API (MOA)
 import com.amazon.aws.moa.jmsobjects.cloudformation.v1_0.Stack;
-import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountAlias;
-import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountOrganizationMembership;
 import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountProvisioningAuthorization;
-import com.amazon.aws.moa.objects.resources.v1_0.AccountAliasQuerySpecification;
-import com.amazon.aws.moa.objects.resources.v1_0.AccountOrganizationMembershipQuerySpecification;
 import com.amazon.aws.moa.objects.resources.v1_0.AccountProvisioningAuthorizationQuerySpecification;
-import com.amazon.aws.moa.objects.resources.v1_0.Datetime;
-import com.amazon.aws.moa.objects.resources.v1_0.Output;
-import com.amazon.aws.moa.objects.resources.v1_0.StackQuerySpecification;
-import com.amazon.aws.moa.objects.resources.v1_0.StackRequisition;
 
 /**
  * An example object provider that maintains an in-memory store of stacks.
@@ -78,7 +58,7 @@ public class ExampleAccountProvisioningAuthorizationProvider extends OpenEaiObje
         // Get the provider properties
         PropertyConfig pConfig = new PropertyConfig();
         try {
-            pConfig = (PropertyConfig) aConfig.getObject("AccountAliasProviderProperties");
+            pConfig = (PropertyConfig) aConfig.getObject("AccountProvisioningAuthorizationProviderProperties");
         } catch (EnterpriseConfigurationObjectException eoce) {
             String errMsg = "Error retrieving a PropertyConfig object from " + "AppConfig: The exception is: " + eoce.getMessage();
             logger.error(LOGTAG + errMsg);
