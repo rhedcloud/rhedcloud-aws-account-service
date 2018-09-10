33fac490b05a7b1700f6dcaf560485ab4d4b7373
Stephen Wheat <swheat@serviceforge.net>
2018-09-10 15:22:12 -0400

    added new step

--- src/main/java/edu/emory/awsaccount/service/provider/step/CreateRsAccountCfnStack.java
+++ src/main/java/edu/emory/awsaccount/service/provider/step/CreateRsAccountCfnStack.java
@@ -14,50 +14,235 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.openeai.config.AppConfig;
+import javax.jms.JMSException;
 
+import org.openeai.config.AppConfig;
+import org.openeai.config.EnterpriseConfigurationObjectException;
+import org.openeai.config.EnterpriseFieldException;
+import org.openeai.jms.producer.MessageProducer;
+import org.openeai.jms.producer.ProducerPool;
+import org.openeai.moa.EnterpriseObjectGenerateException;
+import org.openeai.moa.EnterpriseObjectQueryException;
+import org.openeai.moa.XmlEnterpriseObjectException;
+import org.openeai.transport.RequestService;
+
+import com.amazon.aws.moa.jmsobjects.cloudformation.v1_0.Stack;
+import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountProvisioningAuthorization;
+import com.amazon.aws.moa.objects.resources.v1_0.AccountProvisioningAuthorizationQuerySpecification;
 import com.amazon.aws.moa.objects.resources.v1_0.Property;
-import com.amazon.aws.moa.objects.resources.v1_0.VirtualPrivateCloudRequisition;
+import com.amazon.aws.moa.objects.resources.v1_0.ProvisioningStep;
+import com.amazon.aws.moa.objects.resources.v1_0.StackRequisition;
 
 import edu.emory.awsaccount.service.provider.VirtualPrivateCloudProvisioningProvider;
 
 /**
- * Example step that can serve as a placholder.
+ * If this is a new account request, send an 
+ * AccountProvisioningAuthorization to determine if the user 
+ * is authorized to create a new account.
  * <P>
  * 
  * @author Steve Wheat (swheat@emory.edu)
- * @version 1.0 - 21 May 2017
+ * @version 1.0 - 10 August 2018
  **/
 public class CreateRsAccountCfnStack extends AbstractStep implements Step {
+	
+	private String m_cloudFormationTemplateUrl = null;
+	private ProducerPool m_awsAccountServiceProducerPool = null;
 
 	public void init (String provisioningId, Properties props, 
 			AppConfig aConfig, VirtualPrivateCloudProvisioningProvider vpcpp) 
 			throws StepException {
 		
 		super.init(provisioningId, props, aConfig, vpcpp);
+		
+		String LOGTAG = getStepTag() + "[CreateRsAccountCfnStack.init] ";
+		
+		// Get the custom step properties
+		String cloudFormationTemplateUrl = getProperties()
+				.getProperty("cloudFormationTemplateUrl", null);
+			setCloudFormationTemplateUrl(cloudFormationTemplateUrl);
+			logger.info(LOGTAG + "cloudFormationTemplateUrl is: " + 
+				getCloudFormationTemplateUrl());
+		
+		// This step needs to send messages to the AWS account service
+		// to create stacks.
+		ProducerPool p2p1 = null;
+		try {
+			p2p1 = (ProducerPool)getAppConfig()
+				.getObject("AwsAccountServiceProducerPool");
+			setAwsAccountServiceProducerPool(p2p1);
+		}
+		catch (EnterpriseConfigurationObjectException ecoe) {
+			// An error occurred retrieving an object from AppConfig. Log it and
+			// throw an exception.
+			String errMsg = "An error occurred retrieving an object from " +
+					"AppConfig. The exception is: " + ecoe.getMessage();
+			logger.fatal(LOGTAG + errMsg);
+			throw new StepException(errMsg);
+		}
+		
+		logger.info(LOGTAG + "Initialization complete.");
 	}
 	
 	protected List<Property> run() throws StepException {
 		long startTime = System.currentTimeMillis();
-		String LOGTAG = getStepTag() + "[ExampleStep.run] ";
+		String LOGTAG = getStepTag() + "[CreateRsAccountCfnStack.run] ";
 		logger.info(LOGTAG + "Begin running the step.");
 		
-		// Wait some time.
-		try {
-			Thread.sleep(5000);
-		}
-		catch (InterruptedException ie) {
-			String errMsg = "Error occurred sleeping.";
-			logger.error(LOGTAG + errMsg + ie.getMessage());
-			throw new StepException(errMsg, ie);
-		}
+		boolean isAuthorized = false;
 		
-		// Set return properties.
-		ArrayList<Property> props = new ArrayList<Property>();
+		// Return properties
+		List<Property> props = new ArrayList<Property>();
 		props.add(buildProperty("stepExecutionMethod", RUN_EXEC_TYPE));
 		
+		// Get the allocateNewAccount property from the
+		// DETERMINE_NEW_OR_EXISTING_ACCOUNT step.
+		logger.info(LOGTAG + "Getting properties from preceding steps...");
+		ProvisioningStep step1 = getProvisioningStepByType("DETERMINE_NEW_OR_EXISTING_ACCOUNT");
+		boolean allocateNewAccount = false;
+		if (step1 != null) {
+			logger.info(LOGTAG + "Step DETERMINE_NEW_OR_EXISTING_ACCOUNT found.");
+			String sAllocateNewAccount = getResultProperty(step1, "allocateNewAccount");
+			allocateNewAccount = Boolean.parseBoolean(sAllocateNewAccount);
+			props.add(buildProperty("allocateNewAccount", Boolean.toString(allocateNewAccount)));
+			logger.info(LOGTAG + "Property allocateNewAccount from preceding " +
+				"step is: " + allocateNewAccount);
+		}
+		else {
+			String errMsg = "Step DETERMINE_NEW_OR_EXISTING_ACCOUNT not found. " +
+				"Cannot determine whether or not to authorize the new account " +
+				"requestor.";
+			logger.error(LOGTAG + errMsg);
+			throw new StepException(errMsg);
+		}
+		
+		// Get the newAccountId property from the GENERATE_NEW_ACCOUNT step.
+		logger.info(LOGTAG + "Getting properties from preceding steps...");
+		ProvisioningStep step2 = getProvisioningStepByType("GENERATE_NEW_ACCOUNT");
+		String newAccountId = null;
+		if (step2 != null) {
+			logger.info(LOGTAG + "Step GENERATE_NEW_ACCOUNT found.");
+			newAccountId = getResultProperty(step2, "newAccountId");
+			logger.info(LOGTAG + "Property newAccountId from preceding " +
+				"step is: " + newAccountId);
+			props.add(buildProperty("newAccountId", newAccountId));
+		}
+		else {
+			String errMsg = "Step GENERATE_NEW_ACCOUNT not found. Cannot " +
+				"determine whether or not to authorize the new account " +
+				"requestor.";
+			logger.error(LOGTAG + errMsg);
+			throw new StepException(errMsg);
+		}
+		
+		// If allocateNewAccount is true and newAccountId is not null,
+		// send a Stack.Generate-Request to generate the rs-account stack.
+		if (allocateNewAccount && newAccountId != null) {
+			logger.info(LOGTAG + "allocateNewAccount is true and newAccountId is " + 
+				newAccountId + ". Sending an AccountProvisioningAuthorization." +
+				"Query-Request to determine if the user is authorized to provisiong " +
+				"a new account.");
+			
+			// Send an Stack.Generate-Request. Get a configured Stack and requisition
+			// object from AppConfig.
+			Stack stack = new Stack();
+			StackRequisition req = new StackRequisition();
+		    try {
+		    	stack = (Stack)getAppConfig()
+			    		.getObjectByType(stack.getClass().getName());
+		    	req = (StackRequisition)getAppConfig()
+			    		.getObjectByType(req.getClass().getName());
+		    }
+		    catch (EnterpriseConfigurationObjectException ecoe) {
+		    	String errMsg = "An error occurred retrieving an object from " +
+		    	  "AppConfig. The exception is: " + ecoe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, ecoe);
+		    }
+		    
+		    // Set the values of the requisition.
+		    try {
+		    	req.setAccountId(newAccountId);
+		    	req.setTemplateUrl(getCloudFormationTemplateUrl());
+		    }
+		    catch (EnterpriseFieldException efe) {
+		    	String errMsg = "An error occurred setting the values of the " +
+		  	    	  "requisition. The exception is: " + efe.getMessage();
+		  	    logger.error(LOGTAG + errMsg);
+		  	    throw new StepException(errMsg, efe);
+		    }
+		    
+		    // Log the state of the query spec.
+		    try {
+		    	logger.info(LOGTAG + "Requisition is: " + req.toXmlString());
+		    }
+		    catch (XmlEnterpriseObjectException xeoe) {
+		    	String errMsg = "An error occurred serializing the requisition " +
+		  	    	  "to XML. The exception is: " + xeoe.getMessage();
+	  	    	logger.error(LOGTAG + errMsg);
+	  	    	throw new StepException(errMsg, xeoe);
+		    }    
+			
+			// Get a producer from the pool
+			RequestService rs = null;
+			try {
+				rs = (RequestService)getAwsAccountServiceProducerPool()
+					.getExclusiveProducer();
+			}
+			catch (JMSException jmse) {
+				String errMsg = "An error occurred getting a producer " +
+					"from the pool. The exception is: " + jmse.getMessage();
+				logger.error(LOGTAG + errMsg);
+				throw new StepException(errMsg, jmse);
+			}
+		    
+			List results = null;
+			try { 
+				long generateStartTime = System.currentTimeMillis();
+				results = stack.generate(req, rs);
+				long generateTime = System.currentTimeMillis() - generateStartTime;
+				logger.info(LOGTAG + "Generated CloudFormation Stack in "
+					+ generateTime + " ms. Returned " + results.size() + 
+					" result.");
+			}
+			catch (EnterpriseObjectGenerateException eoge) {
+				String errMsg = "An error occurred generating the  " +
+		    	  "Stack object. The exception is: " + eoge.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, eoge);
+			}
+			finally {
+				// Release the producer back to the pool
+				getAwsAccountServiceProducerPool()
+					.releaseProducer((MessageProducer)rs);
+			}
+			
+			if (results.size() == 1) {
+				Stack stackResult = (Stack)results.get(0);
+				logger.info(LOGTAG + "Stack result is: " + 
+					stackResult.getStackStatus());
+			}
+			else {
+				String errMsg = "Invalid number of results returned from " +
+					"Stack.Generate-Request. " +
+					results.size() + " results returned. Expected exactly 1.";
+				logger.error(LOGTAG + errMsg);
+				throw new StepException(errMsg);
+			}
+		}
+		// If allocateNewAccount is false, log it and add result props.
+		else {
+			logger.info(LOGTAG + "allocateNewAccount is false. " +
+				"no need to authorize the user to create a new account.");
+			props.add(buildProperty("allocateNewAccount", Boolean.toString(allocateNewAccount)));
+			props.add(buildProperty("isAuthorized", "not applicable"));
+		}
+		
 		// Update the step.
-    	update(COMPLETED_STATUS, SUCCESS_RESULT, props);
+		if (allocateNewAccount == false || isAuthorized == true) {
+			update(COMPLETED_STATUS, SUCCESS_RESULT, props);
+		}
+		else update(COMPLETED_STATUS, FAILURE_RESULT, props);
     	
     	// Log completion time.
     	long time = System.currentTimeMillis() - startTime;
@@ -71,13 +256,13 @@ public class CreateRsAccountCfnStack extends AbstractStep implements Step {
 	protected List<Property> simulate() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[ExampleStep.simulate] ";
+			"[CreateRsAccountCfnStack.simulate] ";
 		logger.info(LOGTAG + "Begin step simulation.");
 		
 		// Set return properties.
 		ArrayList<Property> props = new ArrayList<Property>();
-		Property prop = buildProperty("allocateNewAccount", "true");
     	props.add(buildProperty("stepExecutionMethod", SIMULATED_EXEC_TYPE));
+    	Property prop = buildProperty("accountSequenceNumber", "10000");
 		
 		// Update the step.
     	update(COMPLETED_STATUS, SUCCESS_RESULT, props);
@@ -93,7 +278,7 @@ public class CreateRsAccountCfnStack extends AbstractStep implements Step {
 	protected List<Property> fail() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[ExampleStep.fail] ";
+			"[CreateRsAccountCfnStack.fail] ";
 		logger.info(LOGTAG + "Begin step failure simulation.");
 		
 		// Set return properties.
@@ -112,16 +297,41 @@ public class CreateRsAccountCfnStack extends AbstractStep implements Step {
 	}
 	
 	public void rollback() throws StepException {
+		
+		super.rollback();
+		
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[ExampleStep.rollback] ";
+			"[CreateRsAccountCfnStack.rollback] ";
 		logger.info(LOGTAG + "Rollback called, but this step has nothing to " + 
 			"roll back.");
-		update(ROLLBACK_STATUS, SUCCESS_RESULT, null);
+		update(ROLLBACK_STATUS, SUCCESS_RESULT, getResultProperties());
 		
 		// Log completion time.
     	long time = System.currentTimeMillis() - startTime;
     	logger.info(LOGTAG + "Rollback completed in " + time + "ms.");
 	}
 	
+	private void setAwsAccountServiceProducerPool(ProducerPool pool) {
+		m_awsAccountServiceProducerPool = pool;
+	}
+	
+	private ProducerPool getAwsAccountServiceProducerPool() {
+		return m_awsAccountServiceProducerPool;
+	}
+	
+	private void setCloudFormationTemplateUrl (String url) throws 
+		StepException {
+	
+		if (url == null) {
+			String errMsg = "cloudFormationTemplateUrl property is null. " +
+				"Can't continue.";
+			throw new StepException(errMsg);
+		}
+		m_cloudFormationTemplateUrl = url;
+	}
+
+	private String getCloudFormationTemplateUrl() {
+		return m_cloudFormationTemplateUrl;
+	}
 }
