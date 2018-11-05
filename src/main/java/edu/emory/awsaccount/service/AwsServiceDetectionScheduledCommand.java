3f8be34c4bde5827d8e36c2db09d8e5590c1a183
Stephen Wheat <swheat@serviceforge.net>
2018-11-05 09:59:12 -0500

    added initialization test

--- src/main/java/edu/emory/awsaccount/service/AwsServiceDetectionScheduledCommand.java
+++ src/main/java/edu/emory/awsaccount/service/AwsServiceDetectionScheduledCommand.java
@@ -12,6 +12,8 @@
 
 package edu.emory.awsaccount.service;
 
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -22,6 +24,22 @@ import org.openeai.config.CommandConfig;
 import org.openeai.config.EnterpriseConfigurationObjectException;
 import org.openeai.config.PropertyConfig;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.AmazonIdentityManagementException;
+import com.amazonaws.services.identitymanagement.model.ListAccountAliasesResult;
+import com.amazonaws.services.support.AWSSupport;
+import com.amazonaws.services.support.AWSSupportClient;
+import com.amazonaws.services.support.AWSSupportClientBuilder;
+import com.amazonaws.services.support.model.AWSSupportException;
+import com.amazonaws.services.support.model.DescribeServicesRequest;
+import com.amazonaws.services.support.model.DescribeServicesResult;
+import com.amazonaws.services.support.model.Service;
+
+import edu.emory.awsaccount.service.provider.ProviderException;
+
 /**
  * This command interrogate
  * 
@@ -30,7 +48,10 @@ import org.openeai.config.PropertyConfig;
  * 
  */
 public class AwsServiceDetectionScheduledCommand extends AwsAccountScheduledCommand implements ScheduledCommand {
-    private String LOGTAG = "[AwsServiceDetectionScheduledCommand] ";
+   
+	private String LOGTAG = "[AwsServiceDetectionScheduledCommand] ";
+    private String m_accessKeyId = null;
+    private String m_secretKey = null;
 
     public AwsServiceDetectionScheduledCommand(CommandConfig cConfig) throws InstantiationException {
         super(cConfig);
@@ -44,7 +65,8 @@ public class AwsServiceDetectionScheduledCommand extends AwsAccountScheduledComm
             setProperties(props);
         } 
         catch (EnterpriseConfigurationObjectException eoce) {
-            String errMsg = "Error retrieving a PropertyConfig object from " + "AppConfig: The exception is: " + eoce.getMessage();
+            String errMsg = "Error retrieving a PropertyConfig object from "
+            	+ "AppConfig: The exception is: " + eoce.getMessage();
             logger.error(LOGTAG + errMsg);
             throw new InstantiationException(errMsg);
         }
@@ -53,6 +75,58 @@ public class AwsServiceDetectionScheduledCommand extends AwsAccountScheduledComm
         String verbose = getProperties().getProperty("verbose", "false");
         setVerbose(Boolean.getBoolean(verbose));
         logger.info(LOGTAG + "property verbose: " + getVerbose());
+        
+        // Get the AWS credentials the provider will use
+        String accessKeyId = getProperties().getProperty("accessKeyId");
+        if (accessKeyId == null || accessKeyId.equals("")) {
+            String errMsg = "No base accessKeyId property specified. Can't continue.";
+            throw new InstantiationException(errMsg);
+        }
+        setAccessKeyId(accessKeyId);
+
+        String secretKey = getProperties().getProperty("secretKey");
+        if (accessKeyId == null || accessKeyId.equals("")) {
+            String errMsg = "No base secretKey property specified. Can't continue.";
+            throw new InstantiationException(errMsg);
+        }
+        setSecretKey(secretKey);
+        
+        // Create a connection to the master account and query
+        // for all AWS services to demonstrate basic functionality.
+        // Instantiate a basic credential provider
+        logger.info(LOGTAG + "Initializing AWS credential provider...");
+        BasicAWSCredentials creds = new BasicAWSCredentials(accessKeyId, secretKey);
+        AWSStaticCredentialsProvider cp = new AWSStaticCredentialsProvider(creds);
+
+        // Create the Support client
+        logger.info(LOGTAG + "Creating the Support client...");
+        AWSSupport support = AWSSupportClientBuilder.standard().withRegion("us-east-1").withCredentials(cp).build();
+
+        // Query for the list of AWS services to make sure all is working.
+        DescribeServicesResult result = null;
+        try {
+            logger.info(LOGTAG + "Querying for AWS services...");
+            long startTime = System.currentTimeMillis();
+            DescribeServicesRequest request = new DescribeServicesRequest();
+            result = support.describeServices(request);
+            long time = System.currentTimeMillis() - startTime;
+            logger.info(LOGTAG + "Retrieved service list in " + time + " ms.");
+        } catch (AWSSupportException ase) {
+            String errMsg = "An error occured querying for a list of services. " +
+            	"The exception is: " + ase.getMessage();
+            logger.error(LOGTAG + errMsg);
+            throw new InstantiationException(errMsg);
+        }
+
+        List<Service> serviceList = result.getServices();
+        ListIterator it = serviceList.listIterator();
+        logger.info(LOGTAG + "There are presently " + serviceList.size() + " services.");
+        int i=0;
+        while (it.hasNext()) {
+            Service service = (Service) it.next();
+            logger.info(LOGTAG + "Service number " + ++i + 
+            	" is: " + service.getName() + " (" + service.getCode() + ")");
+        }
 
         logger.info(LOGTAG + " Initialization complete.");
     }
@@ -62,6 +136,26 @@ public class AwsServiceDetectionScheduledCommand extends AwsAccountScheduledComm
     	String LOGTAG = "[AwsServiceDetectionScheduledCommand.execute] ";
         long executionStartTime = System.currentTimeMillis();
         logger.info(LOGTAG + "Executing ...");
+        
+        // Query AWS with the Support API for the master list of services.
+        
+        
+        // Query the AWS Account Service for Emory's master list of services.
+        
+        // Iterate over the list of official AWS Services. If any of these 
+        // do not exist in Emory's master list, add them to the list of
+        // new services.
+        
+        // Iterate over the master list of Emory services. If any of these
+        // do not exist in the AWS master list, add them to the list of 
+        // deprecated services.
+        
+        // Iterate over the list of new services and send a
+        // Service.Create-Request for each one.
+        
+        // Iterate over the list of deprecated services and send 
+        // a Service.Update-Request to change the AWS status of
+        // the service to deprecated.
         
         long executionTime = System.currentTimeMillis() - executionStartTime;
         logger.info(LOGTAG + "Command execution completed in " + executionTime + " ms.");
@@ -86,5 +180,43 @@ public class AwsServiceDetectionScheduledCommand extends AwsAccountScheduledComm
      */
     protected boolean getVerbose() {
         return m_verbose;
+    }
+    
+    /**
+     * 
+     * @param String,
+     *            the AWS access key ID to use in client connections
+     * 
+     */
+    private void setAccessKeyId(String accessKeyId) {
+        m_accessKeyId = accessKeyId;
+    }
+
+    /**
+     * 
+     * @return String, the AWS access key ID to use in client connections
+     * 
+     */
+    private String getAccessKeyId() {
+        return m_accessKeyId;
+    }
+
+    /**
+     * 
+     * @param String,
+     *            the AWS secret key to use in client connections
+     * 
+     */
+    private void setSecretKey(String secretKey) {
+        m_secretKey = secretKey;
+    }
+
+    /**
+     * 
+     * @return String, the AWS secret to use in client connections
+     * 
+     */
+    private String getSecretKey() {
+        return m_secretKey;
     }
 }
