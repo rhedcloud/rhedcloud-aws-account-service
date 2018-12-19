6d4cedd830a819ff9ac6db01d5a211fdb28ad8f7
Stephen Wheat <swheat@serviceforge.net>
2018-12-19 17:43:13 -0500

    added a step
    Former-commit-id: 88e62059613a174851d46f209a14b7780d64668f

--- src/main/java/edu/emory/awsaccount/service/provider/step/CreateAccountAlias.java
+++ src/main/java/edu/emory/awsaccount/service/provider/step/CreateAccountAlias.java
@@ -14,27 +14,71 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.openeai.config.AppConfig;
+import javax.jms.JMSException;
 
+import org.openeai.config.AppConfig;
+import org.openeai.config.EnterpriseConfigurationObjectException;
+import org.openeai.config.EnterpriseFieldException;
+import org.openeai.jms.producer.MessageProducer;
+import org.openeai.jms.producer.ProducerPool;
+import org.openeai.moa.EnterpriseObjectCreateException;
+import org.openeai.moa.EnterpriseObjectDeleteException;
+import org.openeai.moa.EnterpriseObjectQueryException;
+import org.openeai.moa.XmlEnterpriseObjectException;
+import org.openeai.moa.objects.resources.Result;
+import org.openeai.transport.RequestService;
+
+import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.Account;
+import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountAlias;
+import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountProvisioningAuthorization;
+import com.amazon.aws.moa.objects.resources.v1_0.AccountProvisioningAuthorizationQuerySpecification;
+import com.amazon.aws.moa.objects.resources.v1_0.AccountQuerySpecification;
+import com.amazon.aws.moa.objects.resources.v1_0.Datetime;
+import com.amazon.aws.moa.objects.resources.v1_0.EmailAddress;
 import com.amazon.aws.moa.objects.resources.v1_0.Property;
+import com.amazon.aws.moa.objects.resources.v1_0.ProvisioningStep;
 import com.amazon.aws.moa.objects.resources.v1_0.VirtualPrivateCloudRequisition;
 
 import edu.emory.awsaccount.service.provider.VirtualPrivateCloudProvisioningProvider;
 
 /**
- * Example step that can serve as a placholder.
+ * If this is a new account request, create account metadata
  * <P>
  * 
  * @author Steve Wheat (swheat@emory.edu)
- * @version 1.0 - 21 May 2017
+ * @version 1.0 - 30 August 2018
  **/
 public class CreateAccountAlias extends AbstractStep implements Step {
+	
+	private ProducerPool m_awsAccountServiceProducerPool = null;
 
 	public void init (String provisioningId, Properties props, 
 			AppConfig aConfig, VirtualPrivateCloudProvisioningProvider vpcpp) 
 			throws StepException {
 		
 		super.init(provisioningId, props, aConfig, vpcpp);
+		
+		String LOGTAG = getStepTag() + "[CreateAccountMetadata.init] ";
+		
+		// This step needs to send messages to the AWS account service
+		// to create account metadata.
+		ProducerPool p2p1 = null;
+		try {
+			p2p1 = (ProducerPool)getAppConfig()
+				.getObject("AwsAccountServiceProducerPool");
+			setAwsAccountServiceProducerPool(p2p1);
+		}
+		catch (EnterpriseConfigurationObjectException ecoe) {
+			// An error occurred retrieving an object from AppConfig. Log it and
+			// throw an exception.
+			String errMsg = "An error occurred retrieving an object from " +
+					"AppConfig. The exception is: " + ecoe.getMessage();
+			logger.fatal(LOGTAG + errMsg);
+			throw new StepException(errMsg);
+		}
+		
+		logger.info(LOGTAG + "Initialization complete.");
+		
 	}
 	
 	protected List<Property> run() throws StepException {
@@ -42,21 +86,128 @@ public class CreateAccountAlias extends AbstractStep implements Step {
 		String LOGTAG = getStepTag() + "[CreateAccountAlias.run] ";
 		logger.info(LOGTAG + "Begin running the step.");
 		
-		// Wait some time.
-		try {
-			Thread.sleep(5000);
-		}
-		catch (InterruptedException ie) {
-			String errMsg = "Error occurred sleeping.";
-			logger.error(LOGTAG + errMsg + ie.getMessage());
-			throw new StepException(errMsg, ie);
-		}
+		boolean accountAliasCreated = false;
 		
-		// Set return properties.
+		// Return properties
 		addResultProperty("stepExecutionMethod", RUN_EXEC_TYPE);
 		
+		// Get some properties from previous steps.
+		String allocateNewAccount = 
+			getStepPropertyValue("GENERATE_NEW_ACCOUNT", "allocateNewAccount");
+		String newAccountId = 
+			getStepPropertyValue("GENERATE_NEW_ACCOUNT", "newAccountId");
+		String name = 
+			getStepPropertyValue("VERIFY_REMAINING_DISTRO_LISTS", "accountAlias");
+		
+		boolean allocatedNewAccount = Boolean.getBoolean(allocateNewAccount);
+		
+		// If allocatedNewAccount is true and newAccountId is not null, 
+		// Send an AccountAlias.Create-Request to the AWS Account service.
+		if (allocatedNewAccount && (newAccountId != null && newAccountId.equalsIgnoreCase("null") == false)) {
+			logger.info(LOGTAG + "allocatedNewAccount is true and newAccountId " + 
+				"is not null. Sending an AccountAlias.Create-Request to create an" +
+				"acount alias.");
+			
+			// Get a configured account object from AppConfig.
+			AccountAlias accountAlias = new AccountAlias();
+		    try {
+		    	accountAlias = (AccountAlias)getAppConfig()
+			    	.getObjectByType(accountAlias.getClass().getName());
+		    }
+		    catch (EnterpriseConfigurationObjectException ecoe) {
+		    	String errMsg = "An error occurred retrieving an object from " +
+		    	  "AppConfig. The exception is: " + ecoe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, ecoe);
+		    }
+		    
+		    // Get the VPCP requisition object.
+		    VirtualPrivateCloudRequisition req = getVirtualPrivateCloudProvisioning()
+		    	.getVirtualPrivateCloudRequisition();
+		    
+		    // Set the values of the account.
+		    try {
+		    	accountAlias.setAccountId(newAccountId);
+		    	accountAlias.setName(name);
+		    }
+		    catch (EnterpriseFieldException efe) {
+		    	String errMsg = "An error occurred setting the values of the " +
+		  	    	  "AccountAlias. The exception is: " + efe.getMessage();
+		  	    logger.error(LOGTAG + errMsg);
+		  	    throw new StepException(errMsg, efe);
+		    }
+		    
+		    // Log the state of the AccountAlias.
+		    try {
+		    	logger.info(LOGTAG + "AccountAlias to create is: " +
+		    		accountAlias.toXmlString());
+		    }
+		    catch (XmlEnterpriseObjectException xeoe) {
+		    	String errMsg = "An error occurred serializing the query spec " +
+		  	    	  "to XML. The exception is: " + xeoe.getMessage();
+	  	    	logger.error(LOGTAG + errMsg);
+	  	    	throw new StepException(errMsg, xeoe);
+		    }    
+			
+			// Get a producer from the pool
+			RequestService rs = null;
+			try {
+				rs = (RequestService)getAwsAccountServiceProducerPool()
+					.getExclusiveProducer();
+			}
+			catch (JMSException jmse) {
+				String errMsg = "An error occurred getting a producer " +
+					"from the pool. The exception is: " + jmse.getMessage();
+				logger.error(LOGTAG + errMsg);
+				throw new StepException(errMsg, jmse);
+			}
+		    
+			try { 
+				long createStartTime = System.currentTimeMillis();
+				accountAlias.create(rs);
+				long createTime = System.currentTimeMillis() - createStartTime;
+				logger.info(LOGTAG + "Create Account in " + createTime +
+					" ms.");
+				accountAliasCreated = true;
+				addResultProperty("allocatedNewAccount", 
+					Boolean.toString(allocatedNewAccount));
+				addResultProperty("accountAliasCreated", 
+					Boolean.toString(accountAliasCreated));
+			}
+			catch (EnterpriseObjectCreateException eoce) {
+				String errMsg = "An error occurred creating the object. " +
+		    	  "The exception is: " + eoce.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, eoce);
+			}
+			finally {
+				// Release the producer back to the pool
+				getAwsAccountServiceProducerPool()
+					.releaseProducer((MessageProducer)rs);
+			}
+			
+		}
+		// If allocatedNewAccount is false, log it and add result props.
+		else {
+			logger.info(LOGTAG + "allocatedNewAccount is false. " +
+				"no need to create account metadata.");
+			addResultProperty("allocatedNewAccount", 
+				Boolean.toString(allocatedNewAccount));
+			addResultProperty("accountAliasCreated", 
+				"not applicable");
+		}
+		
+		// Update the step result.
+		String stepResult = FAILURE_RESULT;
+		if (accountAliasCreated == true && allocatedNewAccount == true) {
+			stepResult = SUCCESS_RESULT;
+		}
+		if (allocatedNewAccount == false) {
+			stepResult = SUCCESS_RESULT;
+		}
+		
 		// Update the step.
-    	update(COMPLETED_STATUS, SUCCESS_RESULT);
+		update(COMPLETED_STATUS, stepResult);
     	
     	// Log completion time.
     	long time = System.currentTimeMillis() - startTime;
@@ -70,12 +221,12 @@ public class CreateAccountAlias extends AbstractStep implements Step {
 	protected List<Property> simulate() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[CreateAccountAlias.simulate] ";
+			"[CreateAccountMetadata.simulate] ";
 		logger.info(LOGTAG + "Begin step simulation.");
 		
 		// Set return properties.
-		Property prop = buildProperty("allocateNewAccount", "true");
     	addResultProperty("stepExecutionMethod", SIMULATED_EXEC_TYPE);
+    	Property prop = buildProperty("accountSequenceNumber", "10000");
 		
 		// Update the step.
     	update(COMPLETED_STATUS, SUCCESS_RESULT);
@@ -91,7 +242,7 @@ public class CreateAccountAlias extends AbstractStep implements Step {
 	protected List<Property> fail() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[CreateAccountAlias.fail] ";
+			"[CreateAccountMetadata.fail] ";
 		logger.info(LOGTAG + "Begin step failure simulation.");
 		
 		// Set return properties.
@@ -109,11 +260,133 @@ public class CreateAccountAlias extends AbstractStep implements Step {
 	}
 	
 	public void rollback() throws StepException {
+		
+		super.rollback();
+		
 		long startTime = System.currentTimeMillis();
-		String LOGTAG = getStepTag() + 
-			"[CreateAccountAlias.rollback] ";
-		logger.info(LOGTAG + "Rollback called, but this step has nothing to " + 
-			"roll back.");
+		String LOGTAG = getStepTag() + "[CreateAccountMetadata.rollback] ";
+		logger.info(LOGTAG + "Rollback called, deleting account metadata.");
+		
+		// Get the result props
+		List<Property> props = getResultProperties();
+		
+		// Get the account number
+		String newAccountId = getResultProperty("newAccountId");
+		
+		// If the newAccountId is not null, query for the account object
+		// and then delete it.
+		if (newAccountId != null) {
+		
+			// Query for the account
+			// Get a configured account object and account query spec
+			// from AppConfig.
+			Account account = new Account();
+			AccountQuerySpecification querySpec = new AccountQuerySpecification();
+		    try {
+		    	account = (Account)getAppConfig()
+			    	.getObjectByType(account.getClass().getName());
+		    	querySpec = (AccountQuerySpecification)getAppConfig()
+				    	.getObjectByType(querySpec.getClass().getName());
+		    }
+		    catch (EnterpriseConfigurationObjectException ecoe) {
+		    	String errMsg = "An error occurred retrieving an object from " +
+		    	  "AppConfig. The exception is: " + ecoe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, ecoe);
+		    }
+		    
+		    // Set the values of the query spec
+		    try {
+		    	querySpec.setAccountId(newAccountId);
+		    }
+		    catch (EnterpriseFieldException efe) {
+		    	String errMsg = "An error occurred setting a field value. " +
+		    		"The exception is: " + efe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException();
+		    }
+		    
+		    // Get a producer from the pool
+ 			RequestService rs = null;
+ 			try {
+ 				rs = (RequestService)getAwsAccountServiceProducerPool()
+ 					.getExclusiveProducer();
+ 			}
+ 			catch (JMSException jmse) {
+ 				String errMsg = "An error occurred getting a producer " +
+ 					"from the pool. The exception is: " + jmse.getMessage();
+ 				logger.error(LOGTAG + errMsg);
+ 				throw new StepException(errMsg, jmse);
+ 			}
+ 		    
+ 			// Query for the account metadata
+ 			List results = null;
+ 			try { 
+ 				long queryStartTime = System.currentTimeMillis();
+ 				results = account.query(querySpec, rs);
+ 				long createTime = System.currentTimeMillis() - queryStartTime;
+ 				logger.info(LOGTAG + "Queried for Account in " + createTime +
+ 					" ms. Got " + results.size() + " result(s).");
+ 			}
+ 			catch (EnterpriseObjectQueryException eoqe) {
+ 				String errMsg = "An error occurred querying for the object. " +
+ 		    	  "The exception is: " + eoqe.getMessage();
+ 		    	logger.error(LOGTAG + errMsg);
+ 		    	throw new StepException(errMsg, eoqe);
+ 			}
+ 			finally {
+ 				// Release the producer back to the pool
+ 				getAwsAccountServiceProducerPool()
+ 					.releaseProducer((MessageProducer)rs);
+ 			}
+ 			
+ 			// If there is a result, delete the account metadata
+ 			if (results.size() > 0) {
+ 				account = (Account)results.get(0);
+ 				
+ 				// Get a producer from the pool
+ 	 			rs = null;
+ 	 			try {
+ 	 				rs = (RequestService)getAwsAccountServiceProducerPool()
+ 	 					.getExclusiveProducer();
+ 	 			}
+ 	 			catch (JMSException jmse) {
+ 	 				String errMsg = "An error occurred getting a producer " +
+ 	 					"from the pool. The exception is: " + jmse.getMessage();
+ 	 				logger.error(LOGTAG + errMsg);
+ 	 				throw new StepException(errMsg, jmse);
+ 	 			}
+ 	 		    
+ 	 			// Delete the account metadata
+ 	 			try { 
+ 	 				long deleteStartTime = System.currentTimeMillis();
+ 	 				account.delete("Delete", rs);
+ 	 				long deleteTime = System.currentTimeMillis() - deleteStartTime;
+ 	 				logger.info(LOGTAG + "Deleted Account in " + deleteTime +
+ 	 					" ms. Got " + results.size() + " result(s).");
+ 	 				addResultProperty("deletedAccountMetadataOnRollback", "true");
+ 	 			}
+ 	 			catch (EnterpriseObjectDeleteException eode) {
+ 	 				String errMsg = "An error occurred deleting the object. " +
+ 	 		    	  "The exception is: " + eode.getMessage();
+ 	 		    	logger.error(LOGTAG + errMsg);
+ 	 		    	throw new StepException(errMsg, eode);
+ 	 			}
+ 	 			finally {
+ 	 				// Release the producer back to the pool
+ 	 				getAwsAccountServiceProducerPool()
+ 	 					.releaseProducer((MessageProducer)rs);
+ 	 			}
+ 			}
+		}
+		// If newAccountId is null, there is nothing to roll back.
+		// Log it.
+		else {
+			logger.info(LOGTAG + "No account metadata was created by this " +
+				"step, so there is nothing to roll back.");
+			addResultProperty("deletedAccountMetadataOnRollback", "not applicable");
+		}
+		
 		update(ROLLBACK_STATUS, SUCCESS_RESULT);
 		
 		// Log completion time.
@@ -121,4 +394,11 @@ public class CreateAccountAlias extends AbstractStep implements Step {
     	logger.info(LOGTAG + "Rollback completed in " + time + "ms.");
 	}
 	
+	private void setAwsAccountServiceProducerPool(ProducerPool pool) {
+		m_awsAccountServiceProducerPool = pool;
+	}
+	
+	private ProducerPool getAwsAccountServiceProducerPool() {
+		return m_awsAccountServiceProducerPool;
+	}
 }
