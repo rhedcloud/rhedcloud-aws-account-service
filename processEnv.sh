323f2c26efb0d4cc304435599044a9afc99b90e6
george wang <george.wang@emory.edu>
2019-09-07 16:27:11 -0400

    added jndi.properties and activemq related jar
    Former-commit-id: 009dc3574f1e15357382e2665c9ce577b462380c

--- processEnv.sh
+++ processEnv.sh
@@ -1,11 +1,12 @@
 unzip -o $BUILD_HOME/resources/axis2-1.5.2-war.zip axis2.war
-jar uf axis2.war -C $BUILD_HOME .ebextensions
+#jar uf axis2.war -C $BUILD_HOME .ebextensions
 mkdir -p WEB-INF/services
 mkdir -p WEB-INF/lib
 mkdir -p WEB-INF/modules
 mkdir -p WEB-INF/conf
 cp rhedcloud-awsaccount-webservice-1.0.aar WEB-INF/services
-cp ../../../build-test/libs/AwsAccountService/* WEB-INF/lib
+cp ../../../build-test/libs/AwsAccountService/*.jar WEB-INF/lib
+cp ../../../build-test/libs/AwsAccountService/jndi.properties WEB-INF/classes
 cp -r ../../../build-test/configs/messaging/Environments/Examples/Jars/AwsAccountService/hibernate/* WEB-INF/classes      
 cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
 cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
