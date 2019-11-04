52324cdd9e49e55808069685e0368c8bb50f591d
George Wang <gw2100@gmail.com>
2019-11-04 19:06:09 +0000

    deploy.sh edited online with Bitbucket
    Former-commit-id: 81d85bbf6ee4bac012df15c7c426c2450f49fda7

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-mvn package
-echo copying
-export TOMCAT_HOME=apache-tomcat-8.0.50
-cp target/*.jar $TOMCAT_HOME/webapps/axis2/WEB-INF/lib
-echo touching
-touch $TOMCAT_HOME/webapps/axis2/WEB-INF/web.xml
