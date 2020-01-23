349d9b752ca1fa51c5476ade6fdac5909d6cf3f3
george wang <george.wang@emory.edu>
2020-01-22 21:47:33 -0500

    added import.sql
    Former-commit-id: 64168e2146f23899033da0776bacdb8428ce0f04

--- processEnv.sh
+++ processEnv.sh
@@ -8,6 +8,7 @@ cp rhedcloud-awsaccount-webservice-1.0.aar WEB-INF/services
 cp ../../../build-test/libs/AwsAccountService/*.jar WEB-INF/lib
 cp ../../../build-test/libs/AwsAccountService/jndi.properties WEB-INF/classes
 cp -r ../../../build-test/configs/messaging/Environments/Examples/Jars/AwsAccountService/hibernate/* WEB-INF/classes      
+cp ../../import.sql WEB-INF/classes
 cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
 cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
 cp ../../../build-test/libs/Axis2/*.jar WEB-INF/lib
