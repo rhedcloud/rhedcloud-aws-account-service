e5b44b1074c53e6edc016e27ff9607e3a88372c3
george wang <george.wang@emory.edu>
2020-01-25 20:48:25 -0500

    trying to hide hibernate.cfg.xml
    Former-commit-id: c7aecde8d217764c9ed9feb1474fb6decfde01ad

--- processEnv.sh
+++ processEnv.sh
@@ -1,5 +1,5 @@
 unzip -o $BUILD_HOME/resources/axis2-1.5.2-war.zip axis2.war
-jar uf axis2.war .ebextensions
+zip uf axis2.war .ebextensions
 mkdir -p WEB-INF/services
 mkdir -p WEB-INF/lib
 mkdir -p WEB-INF/modules
@@ -12,13 +12,13 @@ cp ../../import.sql WEB-INF/classes
 cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
 cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
 cp ../../../build-test/libs/Axis2/*.jar WEB-INF/lib
-jar uf axis2.war WEB-INF/services/rhedcloud-awsaccount-webservice-1.0.aar
-jar uf axis2.war WEB-INF/lib/*
-jar uf axis2.war WEB-INF/classes
-jar uf axis2.war WEB-INF/modules
-jar uf axis2.war WEB-INF/conf
+zip uf axis2.war WEB-INF/services/rhedcloud-awsaccount-webservice-1.0.aar
+zip uf axis2.war WEB-INF/lib/*
+zip uf axis2.war WEB-INF/classes
+zip uf axis2.war WEB-INF/modules
+zip uf axis2.war WEB-INF/conf
 zip -d axis2.war WEB-INF/lib/httpcore-4.0.jar  # conflicts with httpcore-4.4.4.jar from lib folder
 zip -d axis2.war WEB-INF/lib/commons-io-1.4.jar  # conflicts with commons-io-2.5.jar from lib folder
-jar tf axis2.war
+zipinfo axis2.war
 ls axis2.war
 
