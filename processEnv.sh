77ad35b8476ba7205193cce7ed2f03327f349e89
george wang <george.wang@emory.edu>
2020-02-01 18:52:20 -0500

    removed authorization module
    Former-commit-id: 570ca45a4409bbdab29bfcfc8b36619634f511fd

--- processEnv.sh
+++ processEnv.sh
@@ -9,9 +9,9 @@ cp ../../../build-test/libs/AwsAccountService/*.jar WEB-INF/lib
 cp ../../../build-test/libs/AwsAccountService/jndi.properties WEB-INF/classes
 cp -r ../../../build-test/configs/messaging/Environments/Examples/Jars/AwsAccountService/hibernate/* WEB-INF/classes      
 cp ../../import.sql WEB-INF/classes
-cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
-cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
-cp ../../../build-test/libs/Axis2/*.jar WEB-INF/lib
+#cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
+#cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
+#cp ../../../build-test/libs/Axis2/*.jar WEB-INF/lib
 jar uf axis2.war WEB-INF/services/rhedcloud-awsaccount-webservice-1.0.aar
 jar uf axis2.war WEB-INF/lib/*
 jar uf axis2.war WEB-INF/classes
