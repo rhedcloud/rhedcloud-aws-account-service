6a61426cb0cc597bd9dca81922aa3203fe6bf6b8
Darryl L. Pierce <mcpierce@gmail.com>
2020-04-09 15:34:14 -0400

    Add DetermineVpcType provisioning step.
    Former-commit-id: b0e7040bbd3d847292a91230e1eacd3525e54074

---
+++ src/main/java/edu/emory/awsaccount/service/provider/step/DetermineVpcType.java
@@ -0,0 +1,95 @@
+package edu.emory.awsaccount.service.provider.step;
+
+import com.amazon.aws.moa.objects.resources.v1_0.Property;
+
+import java.util.List;
+
+public class DetermineVpcType extends AbstractStep implements Step {
+  private static final String CREATE_VPC_PROPERTY = "createVpc";
+  private static final String VPC_TYPE_PROPERTY = "vpcType";
+
+  @Override
+  protected List<Property> simulate() throws StepException {
+    long startTime = System.currentTimeMillis();
+    String LOGTAG = getStepTag() + "[DetermineVpcType.simulate] ";
+    logger.info(LOGTAG + "Begin step simulation.");
+
+    // Set return properties.
+    addResultProperty("stepExecutionMethod", SIMULATED_EXEC_TYPE);
+
+    // Update the step.
+    update(COMPLETED_STATUS, SUCCESS_RESULT);
+
+    // Log completion time.
+    long time = System.currentTimeMillis() - startTime;
+    logger.info(LOGTAG + "Step simulation completed in " + time + "ms.");
+
+    // Return the properties.
+    return getResultProperties();
+  }
+
+  @Override
+  protected List<Property> run() throws StepException {
+    String LOGTAG = getStepTag() + "[DetermineVpcType.run] ";
+    logger.info(LOGTAG + " Step started");
+    long started = System.currentTimeMillis();
+
+    // get the creating account flag
+    String allocateNewAccount = super.getResultProperty("allocateNewAccount");
+    // get the VPC type flag
+    String vpcType =
+        getVirtualPrivateCloudProvisioning().getVirtualPrivateCloudRequisition().getType();
+
+    logger.debug(LOGTAG + " creatingAccount=" + allocateNewAccount);
+    logger.debug(LOGTAG + " vpcType=" + vpcType);
+
+    if (allocateNewAccount != null && vpcType != null) {
+      // if we're not allocating anew account and the type is 0 then raise an exception
+      if (vpcType.equals("0") && !Boolean.valueOf(allocateNewAccount)) {
+        throw new StepException("Cannot use VPC type 0 when not allocating a new account");
+      }
+
+      // set the result properties for vpc type and create
+      addResultProperty(VPC_TYPE_PROPERTY, vpcType);
+      addResultProperty(CREATE_VPC_PROPERTY, String.valueOf(vpcType.equals("1")));
+    }
+
+    logger.info(LOGTAG + " Step completed in " + (System.currentTimeMillis() - started) + "ms");
+    return getResultProperties();
+  }
+
+  @Override
+  protected List<Property> fail() throws StepException {
+    long startTime = System.currentTimeMillis();
+    String LOGTAG = getStepTag() + "[DetermineVpcType.fail] ";
+    logger.info(LOGTAG + "Begin step failure simulation.");
+
+    // Set return properties.
+    addResultProperty("stepExecutionMethod", FAILURE_EXEC_TYPE);
+
+    // Update the step.
+    update(COMPLETED_STATUS, FAILURE_RESULT);
+
+    // Log completion time.
+    long time = System.currentTimeMillis() - startTime;
+    logger.info(LOGTAG + "Step failure simulation completed in " + time + "ms.");
+
+    // Return the properties.
+    return getResultProperties();
+  }
+
+  @Override
+  public void rollback() throws StepException {
+    String LOGTAG = getStepTag() + "[DetermineVpcType.rollback] ";
+    super.rollback();
+
+    long startTime = System.currentTimeMillis();
+
+    logger.info(LOGTAG + "Rollback called, but this step has nothing to roll back.");
+    update(ROLLBACK_STATUS, SUCCESS_RESULT);
+
+    // Log completion time.
+    long time = System.currentTimeMillis() - startTime;
+    logger.info(LOGTAG + "Rollback completed in " + time + "ms.");
+  }
+}
