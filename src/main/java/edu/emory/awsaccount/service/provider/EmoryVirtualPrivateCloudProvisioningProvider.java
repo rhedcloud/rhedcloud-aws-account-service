3ca6ef0c0da0d9216f1942d01c09cb8631acfaac
Darryl L. Pierce <dpierce@surgeforward.com>
2020-05-05 16:32:31 -0400

    Comment out the remainder of the changes.
    Former-commit-id: 3f350f3c5ebae5180635075ae7e2723344807cad

--- src/main/java/edu/emory/awsaccount/service/provider/EmoryVirtualPrivateCloudProvisioningProvider.java
+++ src/main/java/edu/emory/awsaccount/service/provider/EmoryVirtualPrivateCloudProvisioningProvider.java
@@ -311,11 +311,12 @@ implements VirtualPrivateCloudProvisioningProvider {
 					logger.info(LOGTAG + "Verified class for step "
 						+ stepId +": " + className);
 					logger.info(LOGTAG + "Invoking " + className + ".init() method");
-					VirtualPrivateCloudProvisioning vpcp = (VirtualPrivateCloudProvisioning) getAppConfig()
-							.getObjectByType(this.getClass().getName());
+					// TODO this call isthe one causing an issue
+//					VirtualPrivateCloudProvisioning vpcp = (VirtualPrivateCloudProvisioning) getAppConfig()
+//							.getObjectByType(this.getClass().getName());
 //					step.init(vpcp.getProvisioningId(), props, getAppConfig(),
 //							getVirtualPrivateCloudProvisioningProvider());
-					logger.info(LOGTAG + "Step initialization succeeded");
+//					logger.info(LOGTAG + "Step initialization succeeded");
 				}
 				catch (ClassNotFoundException cnfe) {
 					String errMsg = "An error occurred instantiating " +
@@ -339,11 +340,11 @@ implements VirtualPrivateCloudProvisioningProvider {
 //							"a step. The exception is: " + error.getMessage();
 //					logger.error(LOGTAG + errMsg);
 //					throw new ProviderException(errMsg, error);
-				} catch (EnterpriseConfigurationObjectException error) {
-					String errMsg = "An error occurred instantiating " +
-							"a step. The exception is: " + error.getMessage();
-					logger.error(LOGTAG + errMsg);
-					throw new ProviderException(errMsg, error);
+//				} catch (EnterpriseConfigurationObjectException error) {
+//					String errMsg = "An error occurred instantiating " +
+//							"a step. The exception is: " + error.getMessage();
+//					logger.error(LOGTAG + errMsg);
+//					throw new ProviderException(errMsg, error);
 				}
 			}	
 		}
