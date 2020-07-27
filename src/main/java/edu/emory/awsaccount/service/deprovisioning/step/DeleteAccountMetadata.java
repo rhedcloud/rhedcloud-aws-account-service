7a3ca6237e1ad1c55c31ae8e3e9b3eca59906ae0
Stephen Wheat <swheat@serviceforge.net>
2020-07-27 16:50:01 -0400

    updated a step and added a step
    Former-commit-id: b1b8e4582e75450cb796c988ebafbf141376daaf

--- src/main/java/edu/emory/awsaccount/service/deprovisioning/step/DeleteAccountMetadata.java
+++ src/main/java/edu/emory/awsaccount/service/deprovisioning/step/DeleteAccountMetadata.java
@@ -12,7 +12,6 @@ package edu.emory.awsaccount.service.deprovisioning.step;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Properties;
 
 import javax.jms.JMSException;
@@ -31,27 +30,26 @@ import org.openeai.transport.RequestService;
 
 import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.Account;
 import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.AccountProvisioningAuthorization;
-import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.VirtualPrivateCloud;
+import com.amazon.aws.moa.jmsobjects.provisioning.v1_0.VirtualPrivateCloudProvisioning;
 import com.amazon.aws.moa.objects.resources.v1_0.AccountProvisioningAuthorizationQuerySpecification;
 import com.amazon.aws.moa.objects.resources.v1_0.AccountQuerySpecification;
 import com.amazon.aws.moa.objects.resources.v1_0.Datetime;
 import com.amazon.aws.moa.objects.resources.v1_0.EmailAddress;
 import com.amazon.aws.moa.objects.resources.v1_0.Property;
 import com.amazon.aws.moa.objects.resources.v1_0.ProvisioningStep;
-import com.amazon.aws.moa.objects.resources.v1_0.VirtualPrivateCloudQuerySpecification;
 import com.amazon.aws.moa.objects.resources.v1_0.VirtualPrivateCloudRequisition;
 
 import edu.emory.awsaccount.service.provider.AccountDeprovisioningProvider;
 import edu.emory.awsaccount.service.provider.VirtualPrivateCloudProvisioningProvider;
 
 /**
- * If there is delete 
+ * If there is account metadata, delete it.
  * <P>
  * 
  * @author Steve Wheat (swheat@emory.edu)
- * @version 1.0 - 27 July 2020
+ * @version 1.0 - 30 August 2018
  **/
-public class DeleteVpcMetadata extends AbstractStep implements Step {
+public class DeleteAccountMetadata extends AbstractStep implements Step {
 	
 	private ProducerPool m_awsAccountServiceProducerPool = null;
 
@@ -61,7 +59,7 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 		
 		super.init(provisioningId, props, aConfig, vpcpp);
 		
-		String LOGTAG = getStepTag() + "[DeleteVpcMetadata.init] ";
+		String LOGTAG = getStepTag() + "[DeleteAccountMetadata.init] ";
 		
 		// This step needs to send messages to the AWS account service
 		// to create account metadata.
@@ -89,26 +87,26 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 		String LOGTAG = getStepTag() + "[DeleteAccountMetadata.run] ";
 		
 		// Return properties
-		addResultProperty("stepExecutionMethod", RUN_EXEC_TYPE);		
-				
+		addResultProperty("stepExecutionMethod", RUN_EXEC_TYPE);
 		logger.info(LOGTAG + "Begin running the step.");
-
+		
+		boolean accountMetadataDeleted = false;
+		
 		// Get the accountId from the requisition
 		String accountId = getAccountDeprovisioning()
 			.getAccountDeprovisioningRequisition()
 			.getAccountId();
 		addResultProperty("accountId", accountId);
 		
-		// Query for the VPCs for this accountId
-		// Get a configured VPC object and account query spec
+		// Query for the account
+		// Get a configured account object and account query spec
 		// from AppConfig.
-		VirtualPrivateCloud vpc = new VirtualPrivateCloud();
-		VirtualPrivateCloudQuerySpecification querySpec = 
-			new VirtualPrivateCloudQuerySpecification();
+		Account account = new Account();
+		AccountQuerySpecification querySpec = new AccountQuerySpecification();
 	    try {
-	    	vpc = (VirtualPrivateCloud)getAppConfig()
-		    	.getObjectByType(vpc.getClass().getName());
-	    	querySpec = (VirtualPrivateCloudQuerySpecification)getAppConfig()
+	    	account = (Account)getAppConfig()
+		    	.getObjectByType(account.getClass().getName());
+	    	querySpec = (AccountQuerySpecification)getAppConfig()
 			    	.getObjectByType(querySpec.getClass().getName());
 	    }
 	    catch (EnterpriseConfigurationObjectException ecoe) {
@@ -141,14 +139,14 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 			logger.error(LOGTAG + errMsg);
 			throw new StepException(errMsg, jmse);
 		}
-		
-		// Query for the VPC metadata
+		    
+		// Query for the account metadata
 		List results = null;
 		try { 
 			long queryStartTime = System.currentTimeMillis();
-			results = vpc.query(querySpec, rs);
+			results = account.query(querySpec, rs);
 			long createTime = System.currentTimeMillis() - queryStartTime;
-			logger.info(LOGTAG + "Queried for VPC in " + createTime +
+			logger.info(LOGTAG + "Queried for Account in " + createTime +
 				" ms. Got " + results.size() + " result(s).");
 		}
 		catch (EnterpriseObjectQueryException eoqe) {
@@ -162,67 +160,53 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 			getAwsAccountServiceProducerPool()
 				.releaseProducer((MessageProducer)rs);
 		}
-		
-		// If there is a result, delete the VPC metadata
-		if (results.size() > 0) {
-			String deletedVpcMetadata = "";
-			ListIterator li = results.listIterator();
-			while (li.hasNext()) {
-				vpc = (VirtualPrivateCloud)li.next();
-				
-				// Get a producer from the pool
-	 			rs = null;
-	 			try {
-	 				rs = (RequestService)getAwsAccountServiceProducerPool()
-	 					.getExclusiveProducer();
-	 			}
-	 			catch (JMSException jmse) {
-	 				String errMsg = "An error occurred getting a producer " +
-	 					"from the pool. The exception is: " + jmse.getMessage();
-	 				logger.error(LOGTAG + errMsg);
-	 				throw new StepException(errMsg, jmse);
-	 			}
-	 		    
-	 			// Delete the VPC metadata
-	 			try { 
-	 				long deleteStartTime = System.currentTimeMillis();
-	 				vpc.delete("Delete", rs);
-	 				long deleteTime = System.currentTimeMillis() - deleteStartTime;
-	 				logger.info(LOGTAG + "Deleted VPC with VpcId " + vpc.getVpcId() + 
-	 					"in " + deleteTime + " ms. ");
-	 				if (deletedVpcMetadata.equals("")) {
-	 					deletedVpcMetadata = vpc.getVpcId();
-	 				}
-	 				else {
-	 					deletedVpcMetadata = deletedVpcMetadata + ", " 
-	 						+ vpc.getVpcId();
-	 				}
-	 			}
-	 			catch (EnterpriseObjectDeleteException eode) {
-	 				String errMsg = "An error occurred deleting the object. " +
-	 		    	  "The exception is: " + eode.getMessage();
-	 		    	logger.error(LOGTAG + errMsg);
-	 		    	throw new StepException(errMsg, eode);
-	 			}
-	 			finally {
-	 				// Release the producer back to the pool
-	 				getAwsAccountServiceProducerPool()
-	 					.releaseProducer((MessageProducer)rs);
-	 			}
-			}
 			
-			// Add a result property with the vpcIds of the removed VPC metadata.
-			addResultProperty("deletedVpcMetadata", deletedVpcMetadata);
+		// If there is a result, delete the account metadata
+		if (results.size() > 0) {
+			account = (Account)results.get(0);
+			
+			// Get a producer from the pool
+ 			rs = null;
+ 			try {
+ 				rs = (RequestService)getAwsAccountServiceProducerPool()
+ 					.getExclusiveProducer();
+ 			}
+ 			catch (JMSException jmse) {
+ 				String errMsg = "An error occurred getting a producer " +
+ 					"from the pool. The exception is: " + jmse.getMessage();
+ 				logger.error(LOGTAG + errMsg);
+ 				throw new StepException(errMsg, jmse);
+ 			}
+ 		    
+ 			// Delete the account metadata
+ 			try { 
+ 				long deleteStartTime = System.currentTimeMillis();
+ 				account.delete("Delete", rs);
+ 				long deleteTime = System.currentTimeMillis() - deleteStartTime;
+ 				logger.info(LOGTAG + "Deleted Account in " + deleteTime +
+ 					" ms. Got " + results.size() + " result(s).");
+ 				addResultProperty("deletedAccountMetadata", "true");
+ 			}
+ 			catch (EnterpriseObjectDeleteException eode) {
+ 				String errMsg = "An error occurred deleting the object. " +
+ 		    	  "The exception is: " + eode.getMessage();
+ 		    	logger.error(LOGTAG + errMsg);
+ 		    	throw new StepException(errMsg, eode);
+ 			}
+ 			finally {
+ 				// Release the producer back to the pool
+ 				getAwsAccountServiceProducerPool()
+ 					.releaseProducer((MessageProducer)rs);
+ 			}
+		}
+		else {
+			String msg = "There is no account metadata for account " + accountId +
+				". There is nothing to delete.";
+			logger.info(LOGTAG + msg);
+			addResultProperty("deletedAccountMetadata", "false");
+			addResultProperty("message", "msg");
 		}
 		
-		// There is no VPC metadata, there is nothing to do.
-		else {
-			String msg = "Account " + accountId + 
-				" has no VPC metadata. No VPC metadata to delete.";
-			logger.info(LOGTAG + msg);
-			addResultProperty("message", msg);
-		}
-	
 		// Update the step.
 		update(COMPLETED_STATUS, SUCCESS_RESULT);
 		
@@ -232,18 +216,17 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 		
 		// Return the properties.
 		return getResultProperties();
-		
 	}
 	
 	protected List<Property> simulate() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[DeleteVpcMetadata.simulate] ";
+			"[DeleteAccountMetadata.simulate] ";
 		logger.info(LOGTAG + "Begin step simulation.");
 		
 		// Set return properties.
     	addResultProperty("stepExecutionMethod", SIMULATED_EXEC_TYPE);
-    	addResultProperty("accountMetadataCreated", "true");
+    	addResultProperty("accountMetadataDeleted", "true");
 		
 		// Update the step.
     	update(COMPLETED_STATUS, SUCCESS_RESULT);
@@ -259,7 +242,7 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 	protected List<Property> fail() throws StepException {
 		long startTime = System.currentTimeMillis();
 		String LOGTAG = getStepTag() + 
-			"[DeleteVpcMetadata.fail] ";
+			"[DeleteAccountMetadata.fail] ";
 		logger.info(LOGTAG + "Begin step failure simulation.");
 		
 		// Set return properties.
@@ -281,8 +264,128 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 		super.rollback();
 		
 		long startTime = System.currentTimeMillis();
-		String LOGTAG = getStepTag() + "[DeleteVpcMetadata.rollback] ";
-		logger.info(LOGTAG + "Rollback called, nothing to roll back.");
+		String LOGTAG = getStepTag() + "[CreateAccountMetadata.rollback] ";
+		logger.info(LOGTAG + "Rollback called, deleting account metadata.");
+		
+		// Get the result props
+		List<Property> props = getResultProperties();
+		
+		// Get the account number
+		String newAccountId = getResultProperty("newAccountId");
+		
+		// If the newAccountId is not null, query for the account object
+		// and then delete it.
+		if (newAccountId != null) {
+		
+			// Query for the account
+			// Get a configured account object and account query spec
+			// from AppConfig.
+			Account account = new Account();
+			AccountQuerySpecification querySpec = new AccountQuerySpecification();
+		    try {
+		    	account = (Account)getAppConfig()
+			    	.getObjectByType(account.getClass().getName());
+		    	querySpec = (AccountQuerySpecification)getAppConfig()
+				    	.getObjectByType(querySpec.getClass().getName());
+		    }
+		    catch (EnterpriseConfigurationObjectException ecoe) {
+		    	String errMsg = "An error occurred retrieving an object from " +
+		    	  "AppConfig. The exception is: " + ecoe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException(errMsg, ecoe);
+		    }
+		    
+		    // Set the values of the query spec
+		    try {
+		    	querySpec.setAccountId(newAccountId);
+		    }
+		    catch (EnterpriseFieldException efe) {
+		    	String errMsg = "An error occurred setting a field value. " +
+		    		"The exception is: " + efe.getMessage();
+		    	logger.error(LOGTAG + errMsg);
+		    	throw new StepException();
+		    }
+		    
+		    // Get a producer from the pool
+ 			RequestService rs = null;
+ 			try {
+ 				rs = (RequestService)getAwsAccountServiceProducerPool()
+ 					.getExclusiveProducer();
+ 			}
+ 			catch (JMSException jmse) {
+ 				String errMsg = "An error occurred getting a producer " +
+ 					"from the pool. The exception is: " + jmse.getMessage();
+ 				logger.error(LOGTAG + errMsg);
+ 				throw new StepException(errMsg, jmse);
+ 			}
+ 		    
+ 			// Query for the account metadata
+ 			List results = null;
+ 			try { 
+ 				long queryStartTime = System.currentTimeMillis();
+ 				results = account.query(querySpec, rs);
+ 				long createTime = System.currentTimeMillis() - queryStartTime;
+ 				logger.info(LOGTAG + "Queried for Account in " + createTime +
+ 					" ms. Got " + results.size() + " result(s).");
+ 			}
+ 			catch (EnterpriseObjectQueryException eoqe) {
+ 				String errMsg = "An error occurred querying for the object. " +
+ 		    	  "The exception is: " + eoqe.getMessage();
+ 		    	logger.error(LOGTAG + errMsg);
+ 		    	throw new StepException(errMsg, eoqe);
+ 			}
+ 			finally {
+ 				// Release the producer back to the pool
+ 				getAwsAccountServiceProducerPool()
+ 					.releaseProducer((MessageProducer)rs);
+ 			}
+ 			
+ 			// If there is a result, delete the account metadata
+ 			if (results.size() > 0) {
+ 				account = (Account)results.get(0);
+ 				
+ 				// Get a producer from the pool
+ 	 			rs = null;
+ 	 			try {
+ 	 				rs = (RequestService)getAwsAccountServiceProducerPool()
+ 	 					.getExclusiveProducer();
+ 	 			}
+ 	 			catch (JMSException jmse) {
+ 	 				String errMsg = "An error occurred getting a producer " +
+ 	 					"from the pool. The exception is: " + jmse.getMessage();
+ 	 				logger.error(LOGTAG + errMsg);
+ 	 				throw new StepException(errMsg, jmse);
+ 	 			}
+ 	 		    
+ 	 			// Delete the account metadata
+ 	 			try { 
+ 	 				long deleteStartTime = System.currentTimeMillis();
+ 	 				account.delete("Delete", rs);
+ 	 				long deleteTime = System.currentTimeMillis() - deleteStartTime;
+ 	 				logger.info(LOGTAG + "Deleted Account in " + deleteTime +
+ 	 					" ms. Got " + results.size() + " result(s).");
+ 	 				addResultProperty("deletedAccountMetadataOnRollback", "true");
+ 	 			}
+ 	 			catch (EnterpriseObjectDeleteException eode) {
+ 	 				String errMsg = "An error occurred deleting the object. " +
+ 	 		    	  "The exception is: " + eode.getMessage();
+ 	 		    	logger.error(LOGTAG + errMsg);
+ 	 		    	throw new StepException(errMsg, eode);
+ 	 			}
+ 	 			finally {
+ 	 				// Release the producer back to the pool
+ 	 				getAwsAccountServiceProducerPool()
+ 	 					.releaseProducer((MessageProducer)rs);
+ 	 			}
+ 			}
+		}
+		// If newAccountId is null, there is nothing to roll back.
+		// Log it.
+		else {
+			logger.info(LOGTAG + "No account metadata was created by this " +
+				"step, so there is nothing to roll back.");
+			addResultProperty("deletedAccountMetadataOnRollback", "not applicable");
+		}
 		
 		update(ROLLBACK_STATUS, SUCCESS_RESULT);
 		
@@ -298,5 +401,4 @@ public class DeleteVpcMetadata extends AbstractStep implements Step {
 	private ProducerPool getAwsAccountServiceProducerPool() {
 		return m_awsAccountServiceProducerPool;
 	}
-	
 }
