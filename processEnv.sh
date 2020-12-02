791ddbf20917eaba134996633b8d02d8606d5270
Kevin Hale Boyes <khaleboyes@surgeforward.com>
2020-12-02 14:07:41 -0700

    Move jndi.properties to standard directory layout for maven projects
    Former-commit-id: 6a206d4ae77b228f4e2ac57028644751cdb71fa5

--- processEnv.sh
+++ processEnv.sh
@@ -6,8 +6,7 @@ mkdir -p WEB-INF/modules
 mkdir -p WEB-INF/conf
 cp rhedcloud-awsaccount-webservice-1.0.aar WEB-INF/services
 cp ../../../build-test/libs/AwsAccountService/*.jar WEB-INF/lib
-cp ../../../build-test/libs/AwsAccountService/jndi.properties WEB-INF/classes
-cp -r ../../../build-test/configs/messaging/Environments/Examples/Jars/AwsAccountService/hibernate/* WEB-INF/classes      
+cp -r ../../../build-test/configs/messaging/Environments/Examples/Jars/AwsAccountService/hibernate/* WEB-INF/classes
 cp ../../import.sql WEB-INF/classes
 #cp ../../../build-test/libs/Axis2/openeai-authorization-module.mar WEB-INF/modules
 #cp ../../../build-test/libs/Axis2/axis2.xml WEB-INF/conf
